1fcea8b1ac813af8543f348896f0c97484b26b3b
Simon Lauger <simon@lauger.de>
2026-03-08 01:15:32 +0100

    fix: remove unused int32Ptr helper

--- internal/controller/labels.go
+++ internal/controller/labels.go
@@ -41,5 +41,4 @@ func poolSelector(poolName string) map[string]string {
 }
 
 func int64Ptr(i int64) *int64 { return &i }
-func int32Ptr(i int32) *int32 { return &i }
 func boolPtr(b bool) *bool    { return &b }
